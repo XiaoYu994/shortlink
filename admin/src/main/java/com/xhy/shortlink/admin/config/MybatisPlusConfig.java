9c9546ad4933b1286fd4f7b43d7b6e15c9ede86b
XiaoYu <2489927141@qq.com>
2025-12-02 20:40:02 +0800

    feat(admin): 添加MyBatis-Plus自动填充配置

---
+++ admin/src/main/java/com/xhy/shortlink/admin/config/MybatisPlusConfig.java
@@ -0,0 +1,39 @@
+package com.xhy.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+/**
+ * MyBatis-Plus配置类
+ */
+@Slf4j
+@Configuration
+public class MybatisPlusConfig implements MetaObjectHandler {
+
+    /**
+     * 插入时填充字段
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.debug("插入时自动填充创建时间和更新时间");
+        // 填充创建时间
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        // 填充更新时间
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+
+    /**
+     * 更新时填充字段
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.debug("更新时自动填充更新时间");
+        
+        // 填充更新时间
+        this.strictUpdateFill(metaObject, "updatedTime",Date::new, Date.class);
+    }
+} 
