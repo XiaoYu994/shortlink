9a75f14f82a669ee884642aaac150455e41f0258
XiaoYu <2489927141@qq.com>
2025-12-09 21:22:18 +0800

    feat(config): 添加 MyBatis Plus 分页插件配置

--- project/src/main/java/com/xhy/shortlink/project/config/MybatisPlusConfig.java
+++ project/src/main/java/com/xhy/shortlink/project/config/MybatisPlusConfig.java
@@ -1,8 +1,12 @@
 package com.xhy.shortlink.project.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Date;
@@ -35,5 +39,23 @@ public class MybatisPlusConfig implements MetaObjectHandler {
         
         // 填充更新时间
         this.strictUpdateFill(metaObject, "updateTime",Date::new, Date.class);
+    }
+
+    /**
+     * 配置分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        // 添加分页插件
+        PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInterceptor.setMaxLimit(1000L);
+        // 设置溢出总页数后是否进行处理(默认不处理)
+        paginationInterceptor.setOverflow(false);
+
+        interceptor.addInnerInterceptor(paginationInterceptor);
+        return interceptor;
     }
 } 
