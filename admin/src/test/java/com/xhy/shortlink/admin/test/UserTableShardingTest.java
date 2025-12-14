95257834c829506df6404673fe8984c8690e2ab5
XiaoYu <2489927141@qq.com>
2025-12-14 15:46:34 +0800

    feat(project): 对t_link_access_stats进行分表，并绑定t_link表，避免连表查询出现笛卡尔积问题

--- admin/src/test/java/com/xhy/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/xhy/shortlink/admin/test/UserTableShardingTest.java
@@ -1,36 +1,34 @@
 package com.xhy.shortlink.admin.test;
 
 public class UserTableShardingTest {
-    public static final String SQL ="create table t_link_%d\n" +
+    public static final String SQL ="create table t_link_access_stats_%d" +
             "(\n" +
-            "    id              bigint auto_increment comment 'ID'\n" +
+            "    id             bigint auto_increment comment 'ID'\n" +
             "        primary key,\n" +
-            "    domain          varchar(128)                   null comment '域名',\n" +
-            "    short_uri       varchar(8) collate utf8mb3_bin null comment '短链接',\n" +
-            "    full_short_url  varchar(128)                   null comment '完整短链接',\n" +
-            "    origin_url      varchar(1024)                  null comment '原始链接',\n" +
-            "    click_num       int        default 0           null comment '点击量',\n" +
-            "    gid             varchar(32)                    null comment '分组标识',\n" +
-            "    favicon         varchar(256)                   null comment '网站图标',\n" +
-            "    enable_status   tinyint(1) default 0           null comment '启用标识 （0：启用）（1：未启用）',\n" +
-            "    created_type    tinyint(1)                     null comment '创建类型 0：控制台 1：接口',\n" +
-            "    valid_date_type tinyint(1)                     null comment '有效期类型 0：永久有效 1：用户自定义',\n" +
-            "    valid_date      datetime                       null comment '有效期',\n" +
-            "    `describe`      varchar(1024)                  null comment '描述',\n" +
-            "    `total_pv` int(11) DEFAULT '0' COMMENT '历史PV',\n" +
-            "    `total_uv` int(11) DEFAULT '0' COMMENT '历史UV',\n" +
-            "    `total_uip` int(11) DEFAULT '0' COMMENT '历史UIP',\n" +
-            "    create_time     datetime                       null comment '创建时间',\n" +
-            "    update_time     datetime                       null comment '修改时间',\n" +
-            "    del_flag        tinyint(1) default 0           null comment '删除标识 0：未删除 1：已删除',\n" +
-            "    constraint idx_unique_full_short_url\n" +
-            "        unique (full_short_url)\n" +
+            "    gid            varchar(32)  null comment '分组标识',\n" +
+            "    full_short_url varchar(128) null comment '完整短链接',\n" +
+            "    date           date         null comment '日期',\n" +
+            "    pv             int          null comment '访问量',\n" +
+            "    uv             int          null comment '独立访问数',\n" +
+            "    uip            int          null comment '独立IP数',\n" +
+            "    hour           int          null comment '小时',\n" +
+            "    weekday        int          null comment '星期',\n" +
+            "    create_time    datetime     null comment '创建时间',\n" +
+            "    update_time    datetime     null comment '修改时间',\n" +
+            "    del_flag       tinyint(1)   null comment '删除标识：0 未删除 1 已删除',\n" +
+            "    constraint idx_unique_access_stats\n" +
+            "        unique (full_short_url, gid, hour, date)\n" +
             ")\n" +
-            "    charset = utf8mb4;";
+            "    charset = utf8mb4\n" +
+            "    row_format = DYNAMIC;\n" +
+            "\n" +
+            "create index idx_stats_today\n" +
+            "    on t_link_access_stats_%d (date, full_short_url);\n" +
+            "\n";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
-            System.out.printf((SQL) + "%n", i);
+            System.out.printf(SQL.formatted(i, i));
         }
     }
 }
