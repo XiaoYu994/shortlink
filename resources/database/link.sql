d2aebf53e9ddc1f4f1d9dde720a31b4a86a11862
xiaoyu <2489927141@qq.com>
2026-02-07 19:54:21 +0800

    refactor(database): 优化 t_group 表结构
    合并 t_group_unique 到 t_group，gid 字段添加唯一索引约束

--- resources/database/link.sql
+++ resources/database/link.sql
@@ -17,29 +17,19 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `t_group`
-(
-    `id`          bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-    `gid`         varchar(32)  DEFAULT NULL COMMENT '分组标识',
-    `name`        varchar(64)  DEFAULT NULL COMMENT '分组名称',
-    `username`    varchar(256) DEFAULT NULL COMMENT '创建分组用户名',
-    `sort_order`  int(3) DEFAULT 0 COMMENT '分组排序',
-    `create_time` datetime     DEFAULT NULL COMMENT '创建时间',
-    `update_time` datetime     DEFAULT NULL COMMENT '修改时间',
-    `del_flag`    tinyint(1) DEFAULT 0 COMMENT '删除标识 0：未删除 1：已删除',
-    PRIMARY KEY (`id`),
-    KEY           `idx_username` (`username`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- ----------------------------
--- Table structure for t_group_unique
--- ----------------------------
-CREATE TABLE `t_group_unique`
-(
-    `id`  bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-    `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `idx_unique_gid` (`gid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `t_group` (
+     `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+     `gid` varchar(32) NOT NULL COMMENT '分组标识',
+     `name` varchar(64) DEFAULT NULL COMMENT '分组名称',
+     `username` varchar(256) DEFAULT NULL COMMENT '创建分组用户名',
+     `sort_order` int DEFAULT '0' COMMENT '分组排序',
+     `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+     `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+     `del_flag` tinyint(1) DEFAULT '0' COMMENT '删除标识 0：未删除 1：已删除',
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `idx_unique_gid` (`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 -- ----------------------------
 -- Table structure for t_link
