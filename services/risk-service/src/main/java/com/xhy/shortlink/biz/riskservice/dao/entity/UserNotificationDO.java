861747c059b66c80123839ac08b25561eab593b8
xiaoyu <2489927141@qq.com>
2026-08-19 17:30:30 +0800

    style(risk): 消除通知实体通配导入

--- services/risk-service/src/main/java/com/xhy/shortlink/biz/riskservice/dao/entity/UserNotificationDO.java
+++ services/risk-service/src/main/java/com/xhy/shortlink/biz/riskservice/dao/entity/UserNotificationDO.java
@@ -17,7 +17,11 @@
 
 package com.xhy.shortlink.biz.riskservice.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
